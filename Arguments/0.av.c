26809001182824ff7ac9c26594b4ddfa8ed49a2d
Yadjian92 <10462@holbertonstudents.com>
2025-04-14 14:31:41 +0200

    Add initial implementations for argument printing, PID retrieval, and PID max script

---
+++ Arguments/0.av.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+/**
+ * main - Prints all arguments passed to the program
+ * @ac: The number of arguments passed to the program.
+ * @av: An array of strings containing the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av);
+{
+	int i = 0;
+
+	while (av[i] != NULL)
+	{
+		printf("%s\n", av[i]);
+		i++;
+	}
+	return (0);
+}
